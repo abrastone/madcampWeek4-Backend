90cc03e74ac8fe1321dc86f17b0dd7496664b8ab
abra_stone <abra_stone@naver.com>
2024-07-26 04:29:51 +0000

    final

--- src/app.js
+++ src/app.js
@@ -1,15 +1,40 @@
-import express from "express"
-import userApi from "./api/user/user.js"
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import roomRoutes from './routes/room.js';
 
 const app = express();
 const port = 80;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!!!');
+// Body-parser middleware
+app.use(bodyParser.json());
+
+app.use(cors({
+    origin: "*", // 접근 권한을 부여하는 도메인
+    credentials: true, // 응답 헤더에 Access-Control-Allow-Credentials 추가
+    optionsSuccessStatus: 200, // 응답 상태 200으로 설정
+}));
+
+// MongoDB 연결
+mongoose.connect('mongodb://localhost/constellations', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
-app.use("/user", userApi)
+// 라우트 설정
+app.use('/room', roomRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});  
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
 
 app.listen(port, () => {
-    console.log(`app is now listening on http://localhost:${port}/`);
+    console.log(`Server is running on port ${port}`);
 });
